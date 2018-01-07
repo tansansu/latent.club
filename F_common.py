d152b42c81985623eb061c08957c0153f76a6712
tansansu <freelunacy@naver.com>
2018-01-07 16:19:28 +0900

    페북 제목 추가

--- F_common.py
+++ F_common.py
@@ -83,7 +83,7 @@ def to_md(dataframe, category, directory, page_num):
             meta = '---\ntitle: ' + category + '\nweight: 40\n---\n\n'
         elif category == '가상화폐':
             meta = '---\ntitle: ' + category + '\nweight: 50\n---\n\n'
-        elif category == '트윗':
+        elif category == '트윗/페북':
             meta = '---\ntitle: ' + category + '\nweight: 60\n---\n\n'
         # md 파일에 추가
         with open(md, 'r+') as f:
